701f8840043dd4eda5e076f7ea8131f7067778d4
Ezequiel Martinez <ezeyuske@gmail.com>
2015-05-23 19:45:33 -0300

    Update MaRTA

--- MaRTA/src/MaRTA.c
+++ MaRTA/src/MaRTA.c
@@ -12,31 +12,83 @@
 #include <unistd.h>
 #include <errno.h>
 #include <commons/string.h>
-#include <commons/config.h>
 #include <cxdcommons/general.h>
 #include <cxdcommons/sockets.h>
 
+#define PUERTO_JOBS 9000
+
 int main(int argc, char **argv) {
-	char* config_path = string_new();
-	string_append(&config_path, getAppPath(argv[0]));
-	string_append(&config_path, "/config.cfg");
 	t_config *config;
-	char* ip_fs;
-	int puerto_fs;
+	fd_set master, read_fds;
+	int fd_fs, listener_jobs, newjob, fdmax, addrlen, nbytes, i;
+	char buf[256];
+	struct sockaddr_in remoteaddr;
 
-	if( access( config_path, F_OK ) == -1 ){
-		perror("config_path"); // Error de acceso al archivo
-		exit(1);
+	config = readConfigurationFile(argv);
+
+	FD_ZERO(&master);
+	FD_ZERO(&read_fds);
+
+	listener_jobs = initListener(PUERTO_JOBS);
+	FD_SET(listener_jobs, &master);
+	fdmax = listener_jobs;
+
+	fd_fs = conectarCon(config_get_string_value(config, "IP_FS"), config_get_int_value(config, "PUERTO_FS"));
+	FD_SET(fd_fs, &master);
+	if( fd_fs > fdmax )
+		fdmax = fd_fs;
+
+	// version sin hilos, hay que analizar donde empezar a meter los hilos
+	for(;;){
+		read_fds = master;
+		if( select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1 ){
+			perror("select");
+			exit(1);
+		}
+
+		for(i = 0; i <= fdmax; i++){
+			if( FD_ISSET(i, &read_fds) ){
+				if( i == listener_jobs ){
+					// nueva conexion de un job
+					addrlen = sizeof(remoteaddr);
+					if( ( newjob = accept(listener_jobs, (struct sockaddr *)&remoteaddr, &addrlen) ) == -1 ){
+						perror("accept");
+					}else{
+						FD_SET(newjob, &master);
+						if( newjob > fdmax )
+							fdmax = newjob;
+					}
+				}else if( i == fd_fs ){
+					// FileSystem
+					if( (nbytes = recv(fd_fs, buf, sizeof(buf), 0)) <= 0 ){ // error o conexión cerrada por el FS
+						if( nbytes == 0 ){ // conexión cerrada
+							// aqui deberia volver a conectar
+						}else{
+							perror("recv");
+						}
+						close(fd_fs);
+						FD_CLR(fd_fs, &master);
+					}else{
+						// tenemos mensaje de FS
+					}
+				}else{
+					// Job
+					if( (nbytes = recv(i, buf, sizeof(buf), 0)) <= 0 ){ // error o conexión cerrada por el job
+						if( nbytes == 0 ){ // conexión cerrada
+							// ajam
+						}else{
+							perror("recv");
+						}
+						close(i);
+						FD_CLR(i, &master);
+					}else{
+						// tenemos mensaje de algun job
+					}
+				}
+			}
+		}
 	}
-
-	config = config_create(config_path);
-	free(config_path);
-	ip_fs = config_get_string_value(config, "IP_FS");
-	puerto_fs = config_get_int_value(config, "PUERTO_FS");
-
-	printf("La IP de FS es: %s\n", ip_fs);
-	printf("El puerto de FS es: %d\n", puerto_fs);
-
+	
 	config_destroy(config);
 	return EXIT_SUCCESS;
 }
