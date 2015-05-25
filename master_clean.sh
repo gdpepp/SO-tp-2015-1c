134deaba1f4504dc7a68561bce93598f4a6deeac
Ezequiel Martinez <ezeyuske@gmail.com>
2015-05-25 20:17:58 -0300

    ejecutar antes de hacer un add/push, asi: sh master_clean.sh

---
+++ master_clean.sh
@@ -0,0 +1,8 @@
+cd ~/workspace/tp-2015-1c-compilopordinero/MaRTA/Debug/
+make clean
+cd ~/workspace/tp-2015-1c-compilopordinero/FileSystem/Debug/
+make clean
+cd ~/workspace/tp-2015-1c-compilopordinero/cxd-commons/Debug/
+make clean
+cd ~/workspace/tp-2015-1c-compilopordinero/ProcesoJob/Debug/
+make clean
