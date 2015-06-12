946dc49b9ec5d8b0f651f4e82da5d4d555a3b3ef
gdpepp <dgusti89@gmail.com>
2015-06-12 00:00:39 -0300

    modificacion opcion3.c

--- FileSystem/src/opcion3.c
+++ FileSystem/src/opcion3.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : ABMArchive.c
+ Name        : opcion3.c
  Author      : Compilo x Dinero
  Version     :
  Copyright   : Your copyright notice
@@ -18,13 +18,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <string.h>
 
 int printConsolaOpcion3();
 
 void opcion3() {
 
 	char opcion;
-	char* ruta, ruta2;
+	char ruta[100], ruta2[100];
 	struct stat st = {0};
 
 CONSOLA:
@@ -33,7 +34,7 @@ CONSOLA:
 
 	limpiarPantalla();
 
-	if( opcion != '\n' ){
+	if( opcion != '\n' ) {
 		switch( opcion ) {
 			default:
 				printf("Opción incorrecta...\n");
@@ -43,48 +44,64 @@ CONSOLA:
 		
 			case '1':
 				printf("ingrese ruta a crear: \n");
-				scanf("%s",ruta);
+				scanf("%s", &ruta);
 				if (stat(ruta, &st) == -1) {
-					if((mkdir(ruta, 0700)) != -1)
+					if((mkdir(ruta, 0700)) != -1) {
 						printf("Ruta creada correctamente :)\n");
-					fprintf(stderr, "Ruta '%s' no pudo ser creada: %s\n", ruta, strerror(errno));
-					goto CONSOLA;
-				}
-				else fprintf("La ruta: '%s' definida ya existe. %s\n", ruta, strerror(errno));
+						sleep(1);
+					}
+					else { 
+						fprintf(stderr, "Ruta '%s' no pudo ses creada: %s\n", ruta, strerror(errno));
+						sleep(1); 
+						goto CONSOLA;
+					}
+				} else fprintf("La ruta: '%s' definida ya existe. %s\n", ruta, strerror(errno));
 				goto CONSOLA;
+				break;
 
 			case '2':
 				printf("ingrese ruta a eliminar: \n");
-				scanf("%s",ruta);
-				if (stat(ruta, &st) == -1)
+				scanf("%s", &ruta);
+				if (stat(ruta, &st) == -1) {
 					perror("la ruta no existe \n");
+					sleep(1);
 					goto CONSOLA;
+				}
 				if((rmdir(ruta)) != -1) {
 						printf("Ruta borrada correctamente \n");
+						sleep(1);
 				}
-				else 	fprintf(stderr, "Ruta '%s' no pudo ser borrada: %s\n", ruta, strerror(errno));
-					//desarrollar error handling con errno 
-					//el directorio tiene que estar vacio para eliminarse
-				goto CONSOLA;
-		
+				else {
+					fprintf(stderr, "Ruta '%s' no pudo ser borrada: %s\n", ruta, strerror(errno));
+					sleep(1);
+				}
+					goto CONSOLA;
+
 			case '3':
 				printf("ingrese ruta a renombrar: \n");
-				scanf("%s",ruta);
+				scanf("%s", &ruta);
 				printf("ingrese la nueva ruta: \n");
-				scanf("%s",ruta);
-				if (stat(ruta, &st) == -1)
+				scanf("%s", &ruta);
+				if (stat(ruta, &st) == -1) {
 					perror("la ruta no existe");
+					sleep(1);
 					goto CONSOLA;
-				if((rename(ruta, ruta2)) != -1)
+				}
+				if((rename(ruta, ruta2)) != -1) {
 					printf("la ruta fue renombrada correctamente \n");
-				else fprintf("La ruta: '%s' no pudo ser renombrada. %s\n", ruta, strerror(errno));
+					sleep(1);
+				}
+				else {
+					fprintf("La ruta: '%s' no pudo ser renombrada. %s\n", ruta, strerror(errno));
+					sleep(1);
+				}
 				goto CONSOLA;
 		}
 	} else 	goto CONSOLA;
-}	
+}
 
 int printConsolaOpcion3(void) {
-	pTitulo("Filesystem - Crear/Eliminar/Renombrar/Mover un directorio");
+	pTitulo("Consola de Filesystem - Crear/Eliminar/Renombrar/Mover un directorio");
 	pWhiteLine();
 	pOptionLine('1', "Crear directorio");
 	pOptionLine('2', "Eliminar directorio");
@@ -99,3 +116,13 @@ int printConsolaOpcion3(void) {
 
 	return EXIT_SUCCESS;
 }
+
+/*
+int getlinea(char* ruta) {
+	char c;
+	int i;
+	fflush(0);
+	for(i=0; ((c = getchar()) == '\n'); i++)
+		ruta[i] = c;
+		return EXIT_SUCCESS;
+} */
